bac38d793339c79690c18e36be38b807e871b7d9
s11verz <72031018+s11verz@users.noreply.github.com>
2023-09-12 16:07:27 +0900

    scss 기본

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="body">
+        {/* 중첩 */}
+        <div className="header-grand-parent">
+          grand-parent
+          <div className="header-parent">
+            parent
+            <div className="header-child">child</div>
+          </div>
+        </div>
+
+        {/* 부모 참조 */}
+        <button className="button">Button</button>
+        <button className="button isLoading">Button</button>
+        <button className="button" disabled>
+          Button
+        </button>
+
+        <div className="hello">
+          <span>.hello span</span>
+        </div>
+        <span className="hellospan">.hellospan</span>
+        <br />
+        <span className="hello span">.hello.span</span>
+
+        {/* 변수 */}
+        <div className="variable">변수 적용한 SCSS</div>
+
+        {/* 보간 */}
+        <div className="linear"></div>
+        <div className="linear1">안녕하세요</div>
+        <div className="linear2">안녕하세요</div>
+        <div className="linear3">안녕하세요</div>
+      </div>
+    </>
   );
 }
 
